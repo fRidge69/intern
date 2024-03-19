fa288277a9ede4b8b95054f53d61049a82cd803c
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-03-19 21:02:02 +0530

    fix: guides sort

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'box',
   templateUrl: './box.component.html',
-  styleUrl: './box.component.css'
+  styleUrls: ['./box.component.css']
 })
-export class BoxComponent implements OnInit{
+
+export class BoxComponent {
   number: number = 1;
   selectedDepartment: string | null = null;
   previousDepartment: string | null = null;
@@ -13,22 +14,22 @@ export class BoxComponent implements OnInit{
   selectedGuide: string | null = null;
   previousGuide: string | null = null;
   nextGuide: string | null = null;
+
   guides = [
-    { name: 'Prof. Jagadeesh Bayry', department: 'dept-1' },
-    { name: 'Dr. Abdul Rasheed P', department: 'dept-1' },
-    { name: 'Debarati Chatterjee', department: 'dept-2' },
-    { name: 'Dinesh Jagadeesan', department: 'dept-2' },
-    { name: 'Ankesh Kumar', department: 'dept-3' },
-    { name: 'Arun C O', department: 'dept-3' },
+    { name: 'Prof. Jagadeesh Bayry', department: 'Biological Sciences and Engineering' },
+    { name: 'Dr. Abdul Rasheed P', department: 'Biological Sciences and Engineering' },
+    { name: 'Debarati Chatterjee', department: 'Chemistry' },
+    { name: 'Dinesh Jagadeesan', department: 'Chemistry' },
+    { name: 'Ankesh Kumar', department: 'Civil Engineering' },
+    { name: 'Arun C O', department: 'Civil Engineering' },
     { name: 'Albert Sunny', department: 'dept-4' },
     { name: 'Sandeep Chandran', department: 'dept-4' },
     { name: 'Koninika Pal', department: 'dept-5' },
     { name: 'Mrinal Kanti Das', department: 'dept-5' },
     { name: 'Anirudh Guha', department: 'dept-6' },
     { name: 'Arun Rahul S', department: 'dept-6' }
+  ];
 
-  ]
-  filteredguides: any[]=[];
   increment() {
     this.previousDepartment = this.selectedDepartment;
     this.previousGuide = this.selectedGuide;
@@ -38,6 +39,7 @@ export class BoxComponent implements OnInit{
     this.nextGuide = null;
     this.number++;
   }
+
   decrement() {
     this.nextDepartment = this.selectedDepartment;
     this.nextGuide = this.selectedGuide;
@@ -47,11 +49,20 @@ export class BoxComponent implements OnInit{
     this.previousGuide = null;
     this.number--;
   }
-  ngOnInit(){
-    this.filteredguides=this.guides.filter(
-      guide=> {
-        return (this.guides.department===this.selectedDepartment?this.guides.name)
-      }
-    )
+
+  updateFilteredGuides() {
+    if(this.selectedDepartment==null){
+      return this.guides
+    }
+    return this.guides.filter(guide => guide.department === this.selectedDepartment);
+  }
+
+
+  get filteredGuides(): any[] {
+    console.log("depart",this.selectedDepartment)
+    if(this.selectedDepartment==null){
+      return this.guides
+    }
+    return this.guides.filter(guide => guide.department === this.selectedDepartment);
   }
 }
